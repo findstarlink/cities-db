ad8eeeb5ae51549c29f6ea00e5a05cbc207d72f3
cmdr2 <secondary.cmdr2@gmail.com>
2025-09-23 16:14:30 +0530

    license

--- cityDatabase.js
+++ cityDatabase.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 cmdr2
+ * Copyright (c) 2025 cmdr2
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
